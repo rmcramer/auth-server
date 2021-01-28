4801838970a4a722b376a2dfd795908313c00292
Richard Cramer <richardcramer@richards-air.lan>
2021-01-28 13:08:51 -0500

    finished server

--- router.js
+++ router.js
@@ -1,12 +1,18 @@
 const Authentication = require('./controllers/authentication');
 const passposrtService = require('./services/passport');
 const passport = require('passport');
+// const jwt = require('jwt-simple');
+// const config = require('./config');
 
 const requireAuth = passport.authenticate('jwt',{ session: false });
+const requireSignin = passport.authenticate('local',{ session: false });
 
 module.exports = function(app) {
-  app.post('/signup', Authentication.signup);
-  app.get('/',requireAuth, function(req,res) {
+//  app.get('/',function(req,res) { console.log(jwt.decode(req.headers.authorization,config.secret)); }, requireAuth, function(req,res) {
+  app.get('/', requireAuth, function(req,res) {
     res.send({ hi: 'there' });
   });
+
+  app.post('/signup', Authentication.signup);
+  app.post('/signin', requireSignin, Authentication.signin);
 };
--- services/passport.js
+++ services/passport.js
@@ -3,10 +3,25 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done){
+  //Verify this username/password, call done with user or false
+  User.findOne({email: email}, function(err,user) {
+    if (err) return done(err);
+    if (!user) return done(null,false);
+    user.comparePassword(password,function(err,isMatch){
+      if (err) return done(err);
+      if (!isMatch) return done(null,false);
+      return done(null,user);
+    });
+  });
+});
 
 //Setup options for JWT Strategy
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('Authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
 };
 
@@ -14,7 +29,6 @@ const jwtOptions = {
 const jwtLogin = new JwtStrategy(jwtOptions,function(payload, done) {
   //See if the user ID in the payload exists in our DB, if so,
   //call done with that user, otherwise done with no user object
-  console.log(payload);
   User.findById(payload.sub, function(err,user) {
     if (err) return done(err,false);
 
@@ -28,3 +42,4 @@ const jwtLogin = new JwtStrategy(jwtOptions,function(payload, done) {
 
 //Tell Passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
